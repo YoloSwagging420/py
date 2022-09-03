cc17f1257632f17c32280ac7af8acf7b81cf3365
Aiden Yao <yaoyixuan1992@gmail.com>
2022-09-03 16:28:17 -0400

    123

--- DrYuCode/Coffee Machine/main.py
+++ DrYuCode/Coffee Machine/main.py
@@ -73,7 +73,7 @@ def make_coffee(drink_name, order_ingredients):
 is_on = True
 
 while is_on:
-    choice = input("​What would you like? (espresso/latte/cappuccino): ")
+    choice = input("​What would you like? (espresso $1.5 /latte $2.5 /cappuccino $3.0 ): ")
     if choice == "off":
         is_on = False
     elif choice == "report":
